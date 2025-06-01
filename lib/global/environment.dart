ab49a8323f72e119e00b6ed93ca5e97935d85b9e
Tu Nombre Personal <olivera.m.et13@gmail.com>
2025-06-01 00:47:41 -0300

    FEAT: modify enviroment.dart

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -3,13 +3,13 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 
 class Environment {
   static String apiUrl = kIsWeb
-      ? 'http://localhost:3000/api' 
+      ? 'https://realtime-chat-backend-zf7p.onrender.com/api' 
       : Platform.isAndroid
           ? 'http://10.0.2.2:3000/api'
           : 'http://localhost:3000/api';
 
   static String socketUrl = kIsWeb
-      ? 'http://localhost:3000/socket'
+      ? 'https://realtime-chat-backend-zf7p.onrender.com/socket'
       : Platform.isAndroid
           ? 'http://10.0.2.2:3000/socket'
           : 'http://localhost:3000/socket'; 
